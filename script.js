59f45980e5a4cecd9e630f4472a6f971b0f87e09
aditya <129599872+adityax4@users.noreply.github.com>
2024-06-14 10:12:07 +0530

    add design

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 // javascript
+// scroll test area with mouse wheel
+document.getElementById('text').addEventListener('wheel', function(event) {
+    let textarea = this;
+    let scrollTop = textarea.scrollTop;
+    textarea.scrollTop = scrollTop + event.deltaY;
+    event.preventDefault();
+});
