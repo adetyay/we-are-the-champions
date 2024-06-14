e93a8f3ab2d0a50ac753ffaef9eb28d1e909bcea
aditya <129599872+adityax4@users.noreply.github.com>
2024-06-14 11:28:37 +0530

    connect firebase

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
-// javascript
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://realtime-database-b47ec-default-rtdb.asia-southeast1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const endorsementsListInDB = ref(database, "endorsementList")
+
+const textFieldEl = document.getElementById("text")
+const publishBtn = document.getElementById("btn")
+const endorsementsEl = document.getElementById("endorsements")
+
+publishBtn.addEventListener("click", function(){
+    let textFieldValue = textFieldEl.value
+    console.log(textFieldValue)
+    if(textFieldValue==""){
+        return
+    } else{
+        push(endorsementsListInDB, textFieldValue)
+        clearTextField()
+    }
+})
+
+function clearTextField(){
+    textFieldEl.value = ""
+}
+
+
 // scroll test area with mouse wheel
+
 document.getElementById('text').addEventListener('wheel', function(event) {
     let textarea = this;
     let scrollTop = textarea.scrollTop;
     textarea.scrollTop = scrollTop + event.deltaY;
     event.preventDefault();
 });
+
